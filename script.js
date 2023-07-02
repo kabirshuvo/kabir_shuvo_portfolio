39e3ad1dd73f0e5139c13595852e80cce3a3cb76
kabirshuvo <kabirshuvo19@gmail.com>
2023-07-03 03:10:51 +0600

    Hero text animated

---
+++ script.js
@@ -0,0 +1,25 @@
+const animatedText = document.querySelector(".animated-text")
+
+
+const theBestMe = ["A GOAL SETTER.", "A Father.", "Programer."]
+
+let theBestMeIndex = 0;
+let characterIndex = 0;
+
+updatetheBestMeText()
+
+
+function updatetheBestMeText(){
+    characterIndex++
+    animatedText.innerHTML = `
+    <h1>'${theBestMe[theBestMeIndex].slice(0, characterIndex)}'.</h1>
+    `;
+    if(characterIndex === theBestMe[theBestMeIndex].length){
+        theBestMeIndex++
+        characterIndex = 0;
+    }
+    if(theBestMeIndex === theBestMe.length){
+        theBestMeIndex = 0
+    }
+    setTimeout(updatetheBestMeText, 300)
+}
